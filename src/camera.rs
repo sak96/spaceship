6443544395127820ef94c19330341be510c34d08
sak96 <26397224+sak96@users.noreply.github.com>
2024-11-03 18:23:55 +0530

    Lock camera to spaceship

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::spaceship::SpaceShip;
+
 const CAMERA_DISTANCE: f32 = 80.0;
 
 pub struct CameraPlugin;
@@ -8,6 +10,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(ClearColor(Color::srgb(0.1, 0.0, 0.15)));
         app.add_systems(Startup, spawn_camera);
+        app.add_systems(Update, sync_with_spaceship);
     }
 }
 
@@ -23,4 +26,19 @@ fn spawn_camera(mut commands: Commands) {
         },
         MyCamera,
     ));
+}
+
+fn sync_with_spaceship(
+    space_ship: Query<&Transform, With<SpaceShip>>,
+    mut camera: Query<&mut Transform, (With<MyCamera>, Without<SpaceShip>)>,
+) {
+    let Ok(spaceship) = space_ship.get_single() else {
+        return;
+    };
+    let Ok(mut camera_transform) = camera.get_single_mut() else {
+        return;
+    };
+
+    camera_transform.translation.x = spaceship.translation.x;
+    camera_transform.translation.y = spaceship.translation.y;
 }
