29a9fb35df88f142bbf6b99b0ea6212172684e6e
sak96 <26397224+sak96@users.noreply.github.com>
2024-11-03 17:54:24 +0530

    Remove debug plugin

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use bevy::prelude::*;
 mod asset_loader;
 mod asteroid;
 mod camera;
-mod debug;
 mod movement;
 mod spaceship;
 
@@ -15,7 +14,6 @@ fn main() {
         // user plugins
         .add_plugins(spaceship::SpaceshipPlugin)
         .add_plugins(movement::MovementPlugin)
-        .add_plugins(debug::DebugPlugin)
         .add_plugins(camera::CameraPlugin)
         .add_plugins(asteroid::AsteroidPlugin)
         .add_plugins(asset_loader::AssetLoaderPlugin)
