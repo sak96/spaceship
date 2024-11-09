b58f4e54b2511c96183c7770096660d1ae638e4a
sak96 <26397224+sak96@users.noreply.github.com>
2024-11-09 21:03:27 +0530

    Despawn away entities

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 const CAMERA_DISTANCE: f32 = 80.0;
+const VIEWPORT_MAX_DISTANCE: f32 = 100.0;
 
 pub struct CameraPlugin;
 
@@ -8,6 +9,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(ClearColor(Color::srgb(0.1, 0.0, 0.15)));
         app.add_systems(Startup, spawn_camera);
+        app.add_systems(Update, despawn_entities_out_of_viewport);
     }
 }
 
@@ -23,4 +25,12 @@ fn spawn_camera(mut commands: Commands) {
         },
         MyCamera,
     ));
+}
+
+fn despawn_entities_out_of_viewport(mut commands: Commands, query: Query<(Entity, &Transform)>) {
+    for (entity, transform) in query.iter() {
+        if transform.translation.distance(Vec3::ZERO) > VIEWPORT_MAX_DISTANCE {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
 }
